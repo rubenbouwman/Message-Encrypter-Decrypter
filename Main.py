9f2c3873aa7b7e53cbb44d2685883b62ddc1b178
Ruben Bouwman <rubensb2001@gmail.com>
2024-09-05 17:35:00 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,33 +1,39 @@
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.geometry("500x350")
+root = ctk.CTk()
+root.geometry("500x450")
 
 def EncodeButtonAction():
+    resultTextBox.set
     print("Test ...Encoding")
 
 def DecodeButtonAction():
     print("Test ...Decoding")
 
-frame = customtkinter.CTkFrame(master=root)
+frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Vigenere Cipher")
+label = ctk.CTkLabel(master=frame, text="Vigenere Cipher")
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Message")
+entry1 = ctk.CTkEntry(master=frame, placeholder_text="Message")
 entry1.pack(pady=12, padx=10)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Key", show="*")
+entry2 = ctk.CTkEntry(master=frame, placeholder_text="Key", show="*")
 entry2.pack(pady=12, padx=10)
 
-encodeButton = customtkinter.CTkButton(master=frame, text="Encode message", command=EncodeButtonAction)
+encodeButton = ctk.CTkButton(master=frame, text="Encode message", command=EncodeButtonAction)
 encodeButton.pack(pady=12, padx=10)
 
-decodeButton = customtkinter.CTkButton(master=frame, text="Decode message", command=DecodeButtonAction)
+decodeButton = ctk.CTkButton(master=frame, text="Decode message", command=DecodeButtonAction)
 decodeButton.pack(pady=12, padx=10)
+
+resultTextBox = ctk.CTkTextbox(master=frame, width=250, height=100)
+resultTextBox.configure(state=ctk.DISABLED)
+resultTextBox.pack(pady=12, padx=10)
+
 
 root.mainloop()
