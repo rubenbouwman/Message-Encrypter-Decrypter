3cf1f4dc5706232f6df73e4076ac32f8076ac4b3
Ruben Bouwman <rubensb2001@gmail.com>
2024-09-05 18:18:07 +0200

    Changed the layout of the GUI

--- Main.py
+++ Main.py
@@ -1,39 +1,44 @@
 import customtkinter as ctk
 
+# ----------------- Setup -----------------
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
-
 root = ctk.CTk()
-root.geometry("500x450")
+root.title("Message encrypter & decrypter")
+root.geometry("500x650")
 
-def EncodeButtonAction():
-    resultTextBox.set
-    print("Test ...Encoding")
+# ----------------- Variable Field -----------------
+message = ""
+key = ""
+resultmessage = ""
 
-def DecodeButtonAction():
-    print("Test ...Decoding")
+# ----------------- Controller Functions -----------------
+def EncryptButtonAction():
+    message = message_textBox.get(0.0, "end")
+    print("encrypt test " + message)
 
+def DecryptButtonAction():
+    message = message_textBox.get(0.0, "end")
+    print("decrypt test " + message)
+
+# ----------------- Front-End -----------------
 frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = ctk.CTkLabel(master=frame, text="Vigenere Cipher")
-label.pack(pady=12, padx=10)
-
-entry1 = ctk.CTkEntry(master=frame, placeholder_text="Message")
-entry1.pack(pady=12, padx=10)
+message_textBox = ctk.CTkTextbox(master=frame, width=400)
+message_textBox.pack(pady=20, padx=10)
 
 entry2 = ctk.CTkEntry(master=frame, placeholder_text="Key", show="*")
 entry2.pack(pady=12, padx=10)
 
-encodeButton = ctk.CTkButton(master=frame, text="Encode message", command=EncodeButtonAction)
-encodeButton.pack(pady=12, padx=10)
+encryptButton = ctk.CTkButton(master=frame, text="Encrypt message", command=EncryptButtonAction)
+encryptButton.pack(pady=12, padx=10)
 
-decodeButton = ctk.CTkButton(master=frame, text="Decode message", command=DecodeButtonAction)
-decodeButton.pack(pady=12, padx=10)
+decryptButton = ctk.CTkButton(master=frame, text="Decrypt message", command=DecryptButtonAction)
+decryptButton.pack(pady=12, padx=10)
 
-resultTextBox = ctk.CTkTextbox(master=frame, width=250, height=100)
-resultTextBox.configure(state=ctk.DISABLED)
-resultTextBox.pack(pady=12, padx=10)
+result_textBox = ctk.CTkTextbox(master=frame, width=400)
+result_textBox.pack(pady=20, padx=10)
 
-
+# ----------------- Run -----------------
 root.mainloop()
